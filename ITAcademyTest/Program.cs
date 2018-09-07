3416452f34804bc79f770af556e85267c3a75427
Zeddikus97 <32907529+Zeddikus97@users.noreply.github.com>
2018-09-07 13:23:41 +0300

    Revert "Added classes for character sequences and sequence data collections"
    This reverts commit c8e8efed268520836e914a1b3daba2a62917fd23.

--- ITAcademyTest/Program.cs
+++ ITAcademyTest/Program.cs
@@ -3,12 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using ITAcademyTest.Interfaces;
-using ITAcademyTest.Classes;
-
 
 namespace ITAcademyTest
 {
@@ -16,7 +10,6 @@ namespace ITAcademyTest
     {
         static void Main(string[] args)
         {
-            
         }
     }
 }
