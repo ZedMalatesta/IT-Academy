c133db3c62ec2621093c8807c3e1ed34cdb55885
Zeddikus97 <32907529+Zeddikus97@users.noreply.github.com>
2018-09-07 13:29:11 +0300

    Added classes for character sequences and sequence data collections

---
+++ ITAcademyTest/Classes/SequencesDictionary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITAcademyTest.Interfaces;
+
+namespace ITAcademyTest.Classes
+{
+    class SequencesDictionary
+    {
+        private Dictionary<string, IEnumerable<ISymbolsSequence>> ABIndex { get; set; }
+
+        public SequencesDictionary(Dictionary<string, IEnumerable<ISymbolsSequence>> abindex)
+        {
+            ABIndex = abindex;
+        }
+    }
+}
